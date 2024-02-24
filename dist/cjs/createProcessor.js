544995c21f55bbe3f2a85c698f3a6e6419cf3297
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 06:45:08 -0800

    Upgrade dependencies

--- dist/cjs/createProcessor.js
+++ dist/cjs/createProcessor.js
@@ -62,4 +62,4 @@ function createProcessor(next, options, callback) {
         isProcessing = false;
     };
 }
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { module.exports = exports.default; for (var key in exports) module.exports[key] = exports[key]; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
