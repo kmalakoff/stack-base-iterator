934169ede84a97ed5b57f9944ad9cd57310ff693
Kevin Malakoff <kmalakoff@gmail.com>
2024-04-24 12:17:36 -0700

    Upgrade modules

--- dist/cjs/fifoRemove.js
+++ dist/cjs/fifoRemove.js
@@ -17,4 +17,4 @@ function fifoRemove(fifo, value) {
     }
     return false;
 }
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { module.exports = exports.default; for (var key in exports) module.exports[key] = exports[key]; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
--- dist/cjs/processOrQueue.js
+++ dist/cjs/processOrQueue.js
@@ -32,4 +32,4 @@ function processOrQueue(iterator, callback) {
         if (done && !iterator.done) iterator.end(); // end
     });
 }
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { module.exports = exports.default; for (var key in exports) module.exports[key] = exports[key]; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
