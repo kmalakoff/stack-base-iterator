29211761058075f4ddae6d8b0bc92541643bc5d7
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 20:52:56 -0800

    Replace once with call-once-fn and add pinkie-promise support

--- test/unit/asyncAwait.test.mjs
+++ test/unit/asyncAwait.test.mjs
@@ -1,8 +1,18 @@
 import assert from 'assert';
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+import Promise from 'pinkie-promise';
 import EntriesIterator from '../lib/EntriesIterator.cjs';
 
 describe('asyncAwait', () => {
   if (typeof Symbol === 'undefined' || !Symbol.asyncIterator) return;
+  let globalPromise;
+  before(() => {
+    globalPromise = global.Promise;
+    global.Promise = Promise;
+  });
+  after(() => {
+    global.Promise = globalPromise;
+  });
 
   describe('happy path', () => {
     it('concurrency 1', async () => {
@@ -17,7 +27,7 @@ describe('asyncAwait', () => {
 
         assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
       } catch (err) {
-        assert.ok(!err);
+        assert.ok(!err, err ? err.message : '');
       }
     });
 
@@ -35,7 +45,7 @@ describe('asyncAwait', () => {
 
         assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
       } catch (err) {
-        assert.ok(!err);
+        assert.ok(!err, err ? err.message : '');
       }
     });
   });
