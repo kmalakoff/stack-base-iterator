1c6d24b63e25e9159730429a66506a6827341730
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 14:33:45 -0800

    Remove pinkie-promise and refactor test promise patching

--- test/unit/iterator.test.mjs
+++ test/unit/iterator.test.mjs
@@ -1,7 +1,23 @@
+// biome-ignore lint/suspicious/noShadowRestrictedNames: <explanation>
+import Promise from 'pinkie-promise';
+
 import assert from 'assert';
 import EntriesIterator from '../lib/EntriesIterator.cjs';
 
 describe('iterator', () => {
+  (() => {
+    // patch and restore promise
+    const root = typeof global !== 'undefined' ? global : window;
+    let rootPromise;
+    before(() => {
+      rootPromise = root.Promise;
+      root.Promise = Promise;
+    });
+    after(() => {
+      root.Promise = rootPromise;
+    });
+  })();
+
   describe('happy path', () => {
     it('destroy iterator', () => {
       const iterator = new EntriesIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -46,23 +62,16 @@ describe('iterator', () => {
       );
     });
 
-    it('concurrency 1 - promise', (done) => {
+    it('concurrency 1 - promise', async () => {
       const results = [];
       const iterator = new EntriesIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-      iterator
-        .forEach(
-          (value) => {
-            results.push(value);
-          },
-          { concurrency: 1 }
-        )
-        .then(() => {
-          assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-          done();
-        })
-        .catch((err) => {
-          assert.ok(!err, err ? err.message : '');
-        });
+      await iterator.forEach(
+        (value) => {
+          results.push(value);
+        },
+        { concurrency: 1 }
+      );
+      assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
   });
 
