2a219e84d865fabf385eb6f3179d663f11e85cfa
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 12:30:30 -0800

    Simplify package exports and clean up test suites

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -13,7 +13,7 @@ describe('exports .ts', () => {
       },
       { callbacks: true, concurrency: 1 },
       (err) => {
-        assert.ok(!err, err ? err.message : '');
+        if (err) return done(err);
         assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         done();
       }
