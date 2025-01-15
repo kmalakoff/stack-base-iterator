4258f03443e8dd3534d9543d19dc50c73b2cc5d9
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-14 17:32:43 -0800

    Upgrade modules

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -12,7 +12,7 @@ describe('exports ', () => {
       },
       { callbacks: true, concurrency: 1 },
       (err) => {
-        if (err) return done(err);
+        if (err) return done(err.message);
         assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         done();
       }
--- test/unit/iterator.test.ts
+++ test/unit/iterator.test.ts
@@ -33,7 +33,7 @@ describe('iterator', () => {
         },
         { callbacks: true, concurrency: 1 },
         (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           assert.deepEqual(results, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
           done();
         }
@@ -50,7 +50,7 @@ describe('iterator', () => {
         },
         { callbacks: true, concurrency: Infinity },
         (err) => {
-          if (err) return done(err);
+          if (err) return done(err.message);
           assert.deepEqual(
             results.sort((a, b) => a - b),
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
