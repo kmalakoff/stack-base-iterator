786dafe856ceb32d6712b2d975f77e0738676a9f
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 08:38:38 -0700

    Fixed types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,23 @@
+export type DefaultFunction = (arg1?: unknown, arg2?: unknown) => void;
+export type Callback = (error?: Error, value?: unknown) => void;
+export type EachFunction = (value: unknown, callback?: Callback) => undefined | Promise<unknown>;
+
 export interface StackOptions {
   error?: (err: NodeJS.ErrnoException) => boolean;
 }
 
-export type DefaultFunction = (arg1?: unknown, arg2?: unknown) => void;
+export interface ForEachOptions {
+  error?: (err: NodeJS.ErrnoException) => boolean;
+  callbacks?: boolean;
+  concurrency?: number;
+  limit?: number;
+}
+
+export interface ProcessorOptions extends ForEachOptions {
+  each: EachFunction;
+  counter: number;
+  total: number;
+  stop: (count?: number) => boolean;
+  done?: boolean;
+  err?: Error;
+}
