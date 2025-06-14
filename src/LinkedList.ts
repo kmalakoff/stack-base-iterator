583abe5033e0bc826aa684ea336a895d822e4862
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-14 13:54:48 -0700

    Explicit linked list

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -5,14 +5,10 @@ export default class LinkedList<T> extends FIFO<T> {
   pop(): T {
     return super.pop() as T;
   }
-  // @ts-ignore
-  remove(value: T): boolean {
-    // @ts-ignore - a node called from the base class
-    if (value.list && value.prev && value.next) return super.remove(value);
-
+  removeValue(value: T): boolean {
     for (let node = this.node; node; node = this.next(node)) {
       if (node.value === value) {
-        super.remove(node);
+        this.remove(node);
         return true;
       }
     }
