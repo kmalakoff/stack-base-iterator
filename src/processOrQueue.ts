44670623230a5db4880f1fc50b66806c35972576
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-14 10:37:49 -0700

    Comprehensive types

--- src/processOrQueue.ts
+++ src/processOrQueue.ts
@@ -1,22 +1,31 @@
 import compat from 'async-compat';
+import type FIFO from 'fifo';
 
 import asap from 'asap';
 import fifoRemove from './fifoRemove.js';
 
-export default function processOrQueue(iterator, callback) {
-  if (iterator.done) return callback(null, null);
+import type { DefaultFunction, EachCallback, Iterator } from './types.js';
+
+export default function processOrQueue<T>(iterator: Iterator<T>, callback: EachCallback): undefined {
+  if (iterator.done) {
+    callback(null, null);
+    return;
+  }
 
   // nothing to process so queue
-  if (!iterator.stack.length) return iterator.queued.unshift(callback);
+  if (!iterator.stack.length) {
+    iterator.queued.unshift(callback);
+    return;
+  }
 
   // process next
   const next = iterator.stack.pop();
   iterator.processing.push(callback);
-  next(iterator, function nextCallback(err, result) {
+  next(iterator, function nextCallback(err: Error, result: T) {
     // break call stack
     asap(() => {
       // done is based on stack being empty and not error state as the user may choose to skip the error
-      fifoRemove(iterator.processing, callback);
+      fifoRemove<DefaultFunction>(iterator.processing as unknown as FIFO<DefaultFunction>, callback);
       if (iterator.done) return callback(null, null); // early exit
       if (err && compat.defaultValue(iterator.options.error(err), true)) err = null; // skip error
 
