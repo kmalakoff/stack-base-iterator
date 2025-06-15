f79c2db6ffbfc5d2f48dd3c3095ed33dfe441fda
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-15 12:30:18 -0700

    Removed dependency on fifo

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,28 +1,81 @@
-import FIFO from 'fifo';
+class Node<T> {
+  value: T;
+  prev: Node<T>;
+  next: Node<T>;
+
+  constructor(value: T) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
 
 export default class LinkedList<T> {
-  private fifo = new FIFO<T>();
+  private head: Node<T>;
+  private tail: Node<T>;
+  length: number;
 
-  get length() {
-    return this.fifo.length;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
+
   last(): T {
-    return this.fifo.last();
+    return this.tail ? this.tail.value : undefined;
   }
-  pop(): T {
-    return this.fifo.pop() as T;
-  }
+
   push(value: T): LinkedList<T> {
-    this.fifo.push(value);
+    const newNode = new Node<T>(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
     return this;
   }
-  remove(value: T): boolean {
-    for (let node = this.fifo.node; node; node = this.fifo.next(node)) {
-      if (node.value === value) {
-        this.fifo.remove(node);
-        return true;
-      }
+
+  pop(): T {
+    if (!this.head) return undefined;
+    const poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
     }
-    return false;
+    this.length--;
+    return poppedNode.value;
+  }
+
+  remove(value: T): T {
+    if (!this.head) return undefined;
+
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        if (currentNode === this.head) {
+          this.head = currentNode.next;
+          if (this.head) this.head.prev = null;
+          else this.tail = null;
+        } else if (currentNode === this.tail) {
+          this.tail = currentNode.prev;
+          this.tail.next = null;
+        } else {
+          currentNode.prev.next = currentNode.next;
+          currentNode.next.prev = currentNode.prev;
+        }
+        this.length--;
+        return currentNode.value;
+      }
+      currentNode = currentNode.next;
+    }
+    return undefined;
   }
 }
