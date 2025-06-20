9dd605d2816f34022cbc3b838acf98f181635df3
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:22:02 -0700

    Switch to mocha for unit tests

--- test/unit/asyncIterator.test.ts
+++ test/unit/asyncIterator.test.ts
@@ -6,15 +6,13 @@ describe('asyncIterator', () => {
   if (typeof Symbol === 'undefined' || !Symbol.asyncIterator) return;
   (() => {
     // patch and restore promise
-    // @ts-ignore
-    let rootPromise: Promise;
+    if (typeof global === 'undefined') return;
+    const globalPromise = global.Promise;
     before(() => {
-      rootPromise = global.Promise;
-      // @ts-ignore
       global.Promise = Pinkie;
     });
     after(() => {
-      global.Promise = rootPromise;
+      global.Promise = globalPromise;
     });
   })();
 
