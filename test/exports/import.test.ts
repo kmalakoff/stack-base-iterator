426ed34e1e30cc863e8f06825ae83152bf029688
Kevin Malakoff <kmalakoff@gmail.com>
2025-10-20 21:29:49 -0700

    Upgrade modules

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -5,7 +5,6 @@ describe('exports .ts', () => {
   it('concurrency 1', (done) => {
     const results: number[] = [];
     const iterator = createIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    // @ts-ignore
     iterator.forEach(
       (value: number, callback) => {
         results.push(value);
