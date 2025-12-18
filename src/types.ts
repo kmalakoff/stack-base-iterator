df9ddceb537ac849091fc68be853d690f00f92eb
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 17:26:47 -0800

    Flexible error type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type { EachDoneCallback, EachFunction, ForEachOptions, NextCallback, ProcessCallback, Processor, ProcessorOptions } from 'maximize-iterator';
 export interface StackOptions {
-  error?: (err: NodeJS.ErrnoException) => boolean;
+  error?: (err: NodeJS.ErrnoException) => boolean | void;
 }
 
 export type ValueCallback<T, TReturn = unknown> = (error?: Error, value?: IteratorResult<T, TReturn> | undefined) => void;
